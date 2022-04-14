a692aa46bc5515217ec9bcc14a7c994cc4da26b8
Yassine Sahouane <ysahouane@smb.sqli.com>
2022-04-14 19:07:36 +0000

    v4

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Woopppppooorld!";
+    private final String message = "Hello Wooppxxxxpppooorld!";
 
     public App() {}
 
